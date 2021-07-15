24d986fad220d386a69a9d30af2c9174b9dd7eb6
Beckett Ryan (BFWH) <ryan.beckett@nhs.net>
2021-07-15 09:57:29 +0100

    initial

---
+++ src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  template: `
+    <p>
+      search-bar works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SearchBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
